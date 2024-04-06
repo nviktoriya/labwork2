33cd01a6dfda7e100b328156076ea4a9ae23a06e
nviktoriya <145162470+nviktoriya@users.noreply.github.com>
2024-04-07 02:55:21 +0300

    in master newhello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@
 int
 main()
 {
-  std::string name;
+  std::string name; //введение строковой переменной name
   std::cout << "Enter your name: ";
   std::cin >> name; //ввод с клавиатуры имени
   std::cout << "Hello World from " << name << '!' << endl;
