ab98caa806d00ed78b1e588ea33e793f874809bf
nviktoriya <viktoriyanazarova005@icloud.com>
2024-04-07 02:22:54 +0300

    changed code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
-int main(){
-std::string name;
-std::cout << "Enter your name: ";
-std::cin >> name;//ввод с клавиатуры имени пользователя
-std::cout << "Hello World from " << name << '!' << endl;
-return 0;
+int
+main()
+{
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name; //ввод с клавиатуры имени пользователя
+  std::cout << "Hello World from " << name << '!' << endl;
+  return 0;
 }
